415c727a2965414b9e94c822196d8158131951ad
Shahir Latif <shahirlatif1@gmail.com>
2025-01-18 17:28:09 -0500

    add population counter

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 
@@ -12,7 +13,26 @@ def filter_data(data: pd.DataFrame, x_min, x_max, y_min, y_max):
         (data["X"] > x_min) & (data["X"] < x_max) &
         (data["Y"] > y_min) & (data["Y"] < y_max)]
 
+def count_population(pop_data: pd.DataFrame):
+    
+    x_range = pop_data["X"].max() - pop_data["X"].min()
+    x_dist = x_range * 111.32 * np.cos(np.mean(pop_data["Y"]) * np.pi / 180)
+    
+    y_range = pop_data["Y"].max() - pop_data["Y"].min()
+    y_dist = y_range * 111.32
+
+    area = x_dist * y_dist
+    average_density = pop_data["Z"].mean()
+    return area * average_density
+
+coords_re = re.compile(r"(-?\d+\.?\d*),\s*(-?\d+\.?\d*)")
+def coords_to_xy(coords):
+    if not coords_re.match(coords):
+        raise ValueError("Invalid coordinates")
+    return reversed([float(coord) for coord in coords.split(",")])
+
 if __name__ == "__main__":
     data = read_data()
-    filtered = filter_data(data, -150, -148, 65, 68)
-    print(filtered)
+    # Colorado: -109, -102, 37, 41
+    filtered = filter_data(data, -85.9, -84, 33.9, 34)
+    
